f82f3ec25545921282b391a4b45b63c53536eb36
Myltiane Alves <myltiane.aux@gmail.com>
2022-01-23 13:07:56 -0300

    Adicionando Card

---
+++ src/Components/Card/index.tsx
@@ -0,0 +1,17 @@
+import 
+{
+    Container
+}
+ from './styled';
+
+const Card = () => {
+    return(
+        <>
+          <Container>
+              
+          </Container>  
+        </>
+    )
+}
+
+export default Card;
